ec527d928b4b0dfaaa2ec376150b3df997968574
MCHDF <52521964+MCHDF@users.noreply.github.com>
2020-11-11 20:45:36 +0900

    소개 링크 변경

--- command/about.js
+++ command/about.js
@@ -8,7 +8,7 @@ module.exports = {
         message.reply("개인 DM을 확인해주세요!").then(m => m.delete({ timeout: 3000 }));
         let createdate = dateFormat(bot.user.createdAt, 'yyyy.mm.dd');
         let embed = new MessageEmbed()
-            .setURL("http://mchdfpi.kro.kr/bot/")
+            .setURL("https://github.com/MCHDF")
             .setTitle(`${bot.user.username}에 대해...`)
             .setColor("#FFE4E4")
             .setDescription("여러가지 유틸과 유머 기능, 음악 기능을 탑재한 봇이에요!")
@@ -18,7 +18,7 @@ module.exports = {
             .addField("[ 소유자 ]", "MCHDF#9999", true)
             .addField("[ 지역 ]", ":flag_kr:")
             .addField("[ 생일 ]", createdate,true)
-            .addField("[ Code ]", 'Discord JS v12.2.0',true)
+            .addField("[ Code ]", 'Discord JS v12.4.1',true)
             .addField("[ Node.js ]", 'v12.18.3',true)
             .addField("[ 길드 ]", `**${bot.guilds.cache.size}**개`, true)
             .addField("[ 유저 (중복 포함) ]", `**${bot.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}**명`, true)
