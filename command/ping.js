4b1784998d33e2fccf4ccff7779ca7a6aa11d805
MCHDF <j506661@gmail.com>
2020-11-15 17:06:43 +0900

    메세지 핑, API 핑 분리

--- command/ping.js
+++ command/ping.js
@@ -3,7 +3,7 @@ module.exports = {
         
         const message = await msg.channel.send('🏓 공을 토스해서 서버의 리시브를 기다리고있어요...');
 
-        message.edit(`🏓 리시브를 받았어요!\n걸린 시간 : ${Math.floor(message.createdAt - msg.createdAt)}ms`)
+        message.edit(`🏓 리시브를 받았어요!\n메세지 핑 : ${Math.floor(message.createdTimestamp - msg.createdTimestamp)}ms\nAPI 핑 : ${Math.round(bot.ws.ping)}ms`);
     }
 }
 
