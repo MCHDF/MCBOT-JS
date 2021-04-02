062c6e254a4a0939501d80b3603614169ded8601
MCHDF <j506661@gmail.com>
2021-04-02 09:33:39 +0900

    일일보상 엎어버리기

--- command/dailyReward.js
+++ command/dailyReward.js
@@ -1,5 +1,9 @@
 const { MessageEmbed } = require("discord.js");
-const usedCommand = new Set();
+
+var now = new Date();	// 현재 날짜 및 시간
+var currDate = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}` // 2021-4-2
+var tomorrow = new Date(now.setDate(now.getDate() + 1));	// 내일
+var nextDate = `${tomorrow.getFullYear()}-${tomorrow.getMonth() + 1}-${tomorrow.getDate()}` // 2021-4-3
 
 module.exports = {
     run: async (bot, message, args, con, prefix) => {
@@ -8,41 +12,42 @@ module.exports = {
             if (message.author.id != '468781931182555136') {
                 return message.channel.send('엑세스 거부');
             } else {
-                usedCommand.clear();
+                con.query(`UPDATE Economy SET nextDaily = '${currDate}' WHERE userId = '${message.author.id}';`);
                 return message.channel.send('일일보상 시간 카운팅을 초기화 합니다!');
             }
         } else if (!args[0]) {
             con.query(`SELECT * FROM Economy WHERE userId = '${message.author.id}';`, (err, rows) => {
-                if (!rows[0]) {
-                    return message.channel.send(`유저분은 현재 돈을 가지고있지 않습니다.\n:bulb: \`${prefix}돈 기본지급\``);
-                } else {
-                    if (usedCommand.has(message.author.id)) {
-                        return message.reply(`:arrows_counterclockwise: 다음 일일 보상은 24시간 후에 다시 받으실 수 있어요!`)
-                    } else {
-                        usedCommand.add(message.author.id);
-                        setTimeout(function () {
-                            usedCommand.delete(message.author.id);
-                        }, 86400000);
-                    }
+                if (err) throw err;
+                let nextDaily = rows[0].nextDaily;
+                let money = rows[0].money;
+                let reward = 50000;
 
-                    let dailyReward = 50000;
-                    let money = rows[0].money;
-                    let userId = rows[0].userId;
-                    con.query(`UPDATE Economy SET money = ${money + dailyReward} WHERE userId = '${userId}'`);
+                if(nextDaily === nextDate) {
+                    return message.reply(`이미 보상을 획득하셨습니다. 다음 보상 날짜는 \`${nextDaily}\` 입니다.`);
+                }
+
+                if (currDate != nextDaily || currDate === nextDaily) {
+                    con.query(`UPDATE Economy SET money = ${money + reward} WHERE userId = '${message.author.id}';`);
+                    con.query(`UPDATE Economy SET nextDaily = '${nextDate}' WHERE userId = '${message.author.id}';`);
                     con.query(`SELECT * FROM Economy WHERE userId = '${message.author.id}';`, (err, rows) => {
                         if (err) throw err;
-        
+                        let nextDaily = rows[0].nextDaily;
                         let money = rows[0].money;
-        
+
                         let embed = new MessageEmbed()
-                            .setTitle('일일 보상 획득!')
-                            .addField('[ 일일 보상 ]', '50,000원')
-                            .addField('[ 잔액 ]', `${money.toLocaleString()}원`)
-                            .setTimestamp()
-                            .setColor('GREEN')
-                            .setAuthor(`${message.author.username}`, message.author.displayAvatarURL())
+                        .setTitle('일일보상 획득!')
+                        .setAuthor(message.member.user.username, message.member.user.avatarURL())
+                        .setThumbnail(message.member.user.avatarURL())
+                        .setDescription("일일보상을 획득 하셨습니다!")
+                        .setTimestamp()
+                        .setColor("GREEN")
+                        .addField('[ 보상 ]', `${reward.toLocaleString()}원`)
+                        .addField('[ 잔액 ]', `${money.toLocaleString()}원`)
+                        .addField('[ 다음 보상 ]', `${nextDaily}`)
                         return message.channel.send(embed);
                     })
+                } else {
+                    return message.reply(`이미 보상을 획득하셨습니다. 다음 보상 날짜는 \`${nextDaily}\` 입니다.`);
                 }
             });
         }
