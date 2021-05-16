bbddef490edc22d73ab52eedf384c0d19db21040
MCHDF <j506661@gmail.com>
2021-05-16 14:06:21 +0900

    일부 기능 분리 및 추가

---
+++ config/swearLogger.js
@@ -0,0 +1,39 @@
+const winston = require('winston');
+const winstonDaily = require('winston-daily-rotate-file');
+
+const logDir = `./swearFilterLogs`;
+
+const {
+    combine,
+    timestamp,
+    printf
+} = winston.format;
+
+const logFormat = printf(({
+    level,
+    message,
+    timestamp
+}) => {
+    return `[${timestamp}] [ ${level} ]: ${message}`;
+})
+
+const logger = winston.createLogger({
+    format: combine(
+        timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        logFormat
+    ),
+    transports: [
+        new winstonDaily({
+            level: 'info',
+            datePattern: 'YYYYMMDD',
+            dirname: logDir,
+            filename: `MCBOT_swear_%DATE%.log`,
+            maxFiles: 30,
+            zippedArchive: false
+        })
+    ]
+})
+
+module.exports = logger;
